762c64145382d39d8c8374e97726d883bc6208b2
Julthique <113522369+Julthique@users.noreply.github.com>
2022-10-24 17:50:27 +0200

    Add files via upload

---
+++ supp/supp/supp_event.php
@@ -0,0 +1,27 @@
+<?php
+$id=$_GET['id'];
+$bdd=new PDO('mysql:host=localhost;port=3306;dbname=festival','root','');
+// Requête pour supprimer l'évenement que l'on a choisie ( a l'aide d'un bouton disponible sur sa page admin)//
+$requete='DELETE FROM evenement WHERE id_event='.$id; 
+$resultats = $bdd->query($requete) ;
+
+if ($resultats==true){
+	echo "L'événement a bien été supprimé de la base de donnée du site. <br/><br/>";
+}
+else { echo "L'événement n'a pas pu être supprimé."; }
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="../css/style.css"></link>
+  <title>Supprimer un Événement</title>
+</head>
+<body>
+<form action="../admin.php">
+	<input type="submit" value="Retourner à la page d'accueil !">
+</form>
+</body>
+</html>
