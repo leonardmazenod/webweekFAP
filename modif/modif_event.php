e5c03a71455d7f0b98a775928769bb556b500e88
Julthique <113522369+Julthique@users.noreply.github.com>
2022-10-24 10:11:24 +0200

    Page de modification des événements
    à faire : CSS et Bootstrap

--- modif/modif_event.php
+++ modif/modif_event.php
@@ -7,5 +7,71 @@ $resultats=$connection->query($requete);
 $tab=$resultats->fetch();
 $resultats->closeCursor();
 
-echo "<h1>".$tab[1]."</h1>";
 ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="../style.css"></link>
+  <title>Modif Film</title>
+</head>
+<body>
+	<?php
+	echo '<h1>Modification des informations de l\'Événement : '.$tab[1].'</h1>';
+	?>
+	<form method="POST" action="modifier/modifier_event.php">
+		<?php
+		echo '<input type="text" name="id" value="'.$tab[0].'" hidden>';
+		?>
+		<div>
+			<label for="nom">Nom de l'Événement : </label>
+			<?php
+			echo '<input type="text" name="titre" id="nom" required="required" value="'.$tab[1].'">';
+			?>
+		</div>
+		<div>
+			<label for="xcoord">Coordonée X : </label>
+			<?php
+			echo '<input type="float" id="xcoord" required="required" value="'.$tab[3].'">';	
+			?>
+		</div>
+		<div>
+        <label for="ycoord">Coordonée Y : </label>
+			<?php
+			echo '<input type="float" id="ycoord" required="required" value="'.$tab[4].'">';	
+			?>
+		</div>
+        <div>
+        <label for="description">Description de l'Événement  : </label>
+			<?php
+			echo '<textarea id="description" name="decription "required="required">'.$tab[2].'</textarea>';	
+			?>
+		</div>
+        <div>
+        <label for="horaire">Horaire de l'Événement : </label>
+			<?php
+			echo '<input type="date" id="horaire" required="required" value="'.$tab[6].'">';	
+			?>
+		</div>
+        <div>
+        <label for="duree">Horaire de la fermeture : </label>
+			<?php
+			echo '<input type="text" id="duree" required="required" value="'.$tab[7].'">';	
+			?>
+		</div>
+        <div>
+        <label for="img">Image du Stand : </label>
+			<?php
+			echo '<input type="file" id="img" required="required">';	
+			?>
+		</div>
+		<div>
+			<input type="submit" value="Modifier les informations de l\'Événement !">
+		</div>
+	</form>
+	<form action="../index.php">
+		<input type="submit" value="Annuler">
+	</form>
+</body>
+</html>
