167dff1b2994f1c0c40a8a50c6476c61158deabe
leonardmazenod <116059783+leonardmazenod@users.noreply.github.com>
2022-10-25 15:03:06 +0200

    dossier ajout
    ajout du dossier ajout dans le fap apres avoir ajouter le bootstrap au fichier ajout

---
+++ FAP/ajout/ajout_event.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="../css/style.css">
+  <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+  <title>Ajout Event</title>
+</head>
+<body>
+    <?php include 'navbar.php'; ?>
+    <h1 class="titre_3">Ajout d'un événement :</h1>
+	<form method="POST" action="ajouter/ajouter_event.php">
+        <div class="col-md-6">
+            <label for="nom">Nom de l'événement : </label>
+            <input type="text" name="nom" id="nom" class="form-control" required="required" value="">
+        </div>
+        <div class="row g-3">
+            <div class="col-md-4">
+                <label for="xcoord">Coordonnées X : </label>
+                <input type="float" name="xcoord" id="xcoord" class="form-control" required="required" value="00.00">	
+            </div>
+            <div class="col-md-4">
+                <label for="ycoord">Coordonnées Y : </label>
+                <input type="float" name="ycoord" id="ycoord" class="form-control" required="required" value="00.00">
+            </div>
+            <div class="col-md-8">
+                <label for="description">Description : </label>
+                <textarea id="description" name="description" class="form-control" required="required">... ... ...</textarea>
+            </div>
+        </div>
+        <div class="row g-3">
+            <div class="col-md-4">
+            <label for="horaire">Heure du début de l'évenement : </label>
+                <input type="text" name="horaire" id="horaire" class="form-control" required="required" value="2022-01-01 12:00:00">
+            </div>
+            <div class="col-md-4">
+            <label for="duree">Durée de l'événement : </label>
+                <input type="text" name="duree" id="duree" class="form-control" required="required" value="02:00:00">
+            </div>
+        </div>
+        <div class="row g-3">
+            <div class="col-md-4">
+                <label for="nbplace">Nombre de place à l'événement : </label>
+                <input type="number" name="nbplace" id="nbplace" class="form-control" required="required">
+            </div>
+            <div class="col-md-4">
+                <label for="img">Image de l'événement : </label>
+                <input type="file" name="img" id="img" class="form-control" required="required">
+            </div>
+        </div>
+            <div>
+                <input type="submit" class="btn btn-primary" value="Ajouter l'événement !">
+            </div>
+	</form>
+	
+	<form action="../admin.php">
+		<input type="submit" class="btn btn-primary" value="Annuler">
+	</form>
+</body>
+</html>
---
+++ FAP/ajout/ajouter/ajouter_event.php
@@ -0,0 +1,47 @@
+<?php
+$connection=new PDO('mysql:host=localhost;port=3306;dbname=festival','root','');
+$nomevent=$_POST['nom']; 
+$xcoordevent=$_POST['xcoord']; 
+$ycoordevent=$_POST['ycoord']; 
+$defevent=$_POST['description']; 
+$horaireevent=$_POST['horaire']; 
+$dureeevent=$_POST['duree']; 
+$imgevent=$_POST['img'];
+$nbplace=$_POST['nbplace'];
+
+
+//	Requête qui permet d'insérer toutes les informations que l'on a récupéré dans le formlaire sur le nouveau film	et de les insérer dans la base de données // 
+$reqpreparee = $connection->prepare("INSERT INTO evenement(nomevent,defevent,xcoordevent,ycoordevent,imgevent,horaireevent,dureeevent,nbplace) VALUES(:nomevent,:defevent,:xcoordevent,:ycoordevent,:imgevent,:horaireevent,:dureeevent,:nbplace)");
+$reqpreparee->bindParam(':nomevent', $nomevent); 
+$reqpreparee->bindParam(':xcoordevent', $xcoordevent);
+$reqpreparee->bindParam(':ycoordevent', $ycoordevent); 
+$reqpreparee->bindParam(':defevent', $defevent); 
+$reqpreparee->bindParam(':horaireevent', $horaireevent); 
+$reqpreparee->bindParam(':dureeevent', $dureeevent); 
+$reqpreparee->bindParam(':imgevent', $imgevent); 
+$reqpreparee->bindParam(':nbplace', $nbplace); 
+
+
+$succes=$reqpreparee->execute();
+
+if($succes==true){
+	echo ' Information du nouvel événement ajoutée.';
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="../../css/style.css"></link>
+  <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+  <title>Ajouter un événement</title>
+</head>
+<body>
+<?php include 'navbar.php'; ?>
+<form action="../../admin.php">
+	<input type="submit" class="btn btn-primary" value="Retourner à la page d'accueil !">
+</form>
+</body>
