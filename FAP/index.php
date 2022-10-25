94a9f089cea4a86b22e215aaabe9f29f2dd8ee16
maximetincani <116059733+maximetincani@users.noreply.github.com>
2022-10-25 08:43:51 +0200

    correction navbar
    ajout page formulaire et navbar, convertion en PHP

---
+++ FAP/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content=width=device-width, initial-scale=1>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <title>Festival de l'Artisanat au Puy</title>
+        <link rel="icon" href="img/icon.png">
+        <link rel="stylesheet" href="css/style.css">
+        <script src="https:/kit.fontawesome.com/37f50f5103.js" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.6.1.js"></script> 
+    </head>
+    <body>
+    <!-- Navbar -->
+    <?php 
+        include("navbar.php"); 
+    ?>
+
+    <!-- Titre -->
+    <div class="titre">
+        <h1>Festival de l’Artisanat </br>au Puy-en-Velay </h1>
+        <h3>du 08 au 10 avril 23</h3>
+        <div class="container">
+            <button>voir les activités</button>
+            <i class="fa-solid fa-arrow-down fa-beat fa-xl"></i>
+        </div>
+    </div>
+
+    <!-- Sous-titre -->
+    <div class="sous-titre">
+        <h2>Des artisants de toute l’Europe se réunissent pour tenir un festival au Puy-en-Velay du 8 au 10 avril 2023</h2>
+        <button>en savoir plus sur le festival</button>
+    </div>
+
+    <!-- Cartes -->
+    <div class="cartes">
+        <div class="carte">
+            <img src="img/1.png" alt="Céramiste">
+            <div class="sous-texte">
+                <h4>Céramiste</h4>
+                <p>George Rempart</br>retrouvé le à son atelier</br>de 8h à 19h du samedi au lundi</p>
+                <a href="#">voir plus</a>
+            </div>
+        </div>
+        <div class="carte">
+            <img src="img/1.png" alt="Céramiste">
+            <div class="sous-texte">
+                <h4>Céramiste</h4>
+                <p>George Rempart</br>retrouvé le à son atelier</br>de 8h à 19h du samedi au lundi</p>
+                <a href="#">voir plus</a>
+            </div>
+        </div>
+        <div class="carte">
+            <img src="img/1.png" alt="Céramiste">
+            <div class="sous-texte">
+                <h4>Céramiste</h4>
+                <p>George Rempart</br>retrouvé le à son atelier</br>de 8h à 19h du samedi au lundi</p>
+                <a href="#">voir plus</a>
+            </div>
+        </div>
+    </div>
+
+        <a href="#"><i class="fa-brands fa-instagram"></i></a>
+
+        <script src="js/script.js"></script>
+    </body>
+</html>
