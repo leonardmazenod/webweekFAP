e5277eec8192f51c7d8b81873a7f68e02007d7f0
Julthique <113522369+Julthique@users.noreply.github.com>
2022-10-25 09:39:03 +0200

    Ajout des pages pour ajouter des éléments à la BDD

---
+++ ajout/ajout_boutique.php
@@ -0,0 +1,65 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="../style.css"></link>
+  <title>Modif Film</title>
+</head>
+<body>
+
+    <h1>Ajout d'une Boutique :</h1>
+	<form method="POST" action="ajouter/ajouter_boutique.php">
+		<div>
+			<label for="nom">Nom de la boutique : </label>
+            <input type="text" name="nom" id="nom" required="required" value="">
+		</div><br/>
+		<div>
+			<label for="type">Type de la boutique :</label>
+			<select class="form-select" name="type" aria-label="selecttype">
+                <option selected disabled>Choisir un type</option>
+                <option value="textile">Textile</option>
+                <option value="sculpture">Sculpture</option>
+                <option value="bois">Travail du bois et des plantes</option>
+                <option value="autre">Autre</option>
+                </select>
+		</div><br/>
+		<div>
+			<label for="xcoord">Coordonée X : </label>
+			<input type="float" name="xcoord" id="xcoord" required="required" value="00.00">	
+		</div><br/>
+		<div>
+        <label for="ycoord">Coordonée Y : </label>
+            <input type="float" name="ycoord" id="ycoord" required="required" value="00.00">
+		</div><br/>
+        <div>
+        <label for="description">Description : </label>
+			<textarea id="description" name="description" required="required">... ... ...</textarea>
+		</div><br/>
+        <div>
+        <label for="ouverture">Horaire de l'ouverture : </label>
+			<input type="text" name="ouverture" id="ouverture" required="required" value="00:00:00">
+		</div><br/>
+        <div>
+        <label for="fermeture">Horaire de la fermeture : </label>
+			<input type="text" name="fermeture" id="fermeture" required="required" value="00:00:00">	
+		</div><br/>
+        <div>
+        <label for="tel">téléphone de la Boutique : </label>
+			<input type="text" name="tel" id="tel" required="required" value="0601020304">
+		</div><br/>
+        <div>
+        <label for="img">Image de la Boutique : </label>
+			<input type="file" name="img" id="img" required="required">
+		</div><br/>
+		<div>
+			<input type="submit" value="Ajouter la Boutique !">
+		</div>
+	</form>
+	<br/>
+	<form action="../admin.php">
+		<input type="submit" value="Annuler">
+	</form>
+</body>
+</html>
---
+++ ajout/ajouter/ajouter_stand.php
@@ -0,0 +1,46 @@
+<?php
+$connection=new PDO('mysql:host=localhost;port=3306;dbname=festival','root','');
+$nomstand=$_POST['nom']; 
+$typestand=$_POST['type']; 
+$xcoordstand=$_POST['xcoord']; 
+$ycoordstand=$_POST['ycoord']; 
+$descriptionstand=$_POST['description']; 
+$ouverturestand=$_POST['ouverture']; 
+$fermeturestand=$_POST['fermeture']; 
+$emplacement=$_POST['emplacement']; 
+$imgstand=$_POST['img'];
+
+
+//	Requête qui permet d'insérer toutes les informations que l'on a récupéré dans le formlaire sur le nouveau film	et de les insérer dans la base de données // 
+$reqpreparee = $connection->prepare("INSERT INTO stand(nomstand,typestand,xcoordstand,ycoordstand,descriptionstand,imgstand,ouverturestand,fermeturestand,emplacement) VALUES(:nomstand,:typestand,:xcoordstand,:ycoordstand,:descriptionstand,:imgbouti,:ouverturestand,:fermeturestand,:emplacement)");
+$reqpreparee->bindParam(':nomstand', $nomstand); 
+$reqpreparee->bindParam(':typestand', $typestand);
+$reqpreparee->bindParam(':xcoordstand', $xcoordstand); 
+$reqpreparee->bindParam(':ycoordstand', $ycoordstand); 
+$reqpreparee->bindParam(':descriptionstand', $descriptionstand); 
+$reqpreparee->bindParam(':imgbouti', $imgbouti); 
+$reqpreparee->bindParam(':ouverturestand', $ouverturestand); 
+$reqpreparee->bindParam(':fermeturestand', $fermeturestand); 
+$reqpreparee->bindParam(':emplacement', $emplacement); 
+
+$succes=$reqpreparee->execute();
+
+if($succes==true){
+	echo ' Information du nouveaux stand ajouté';
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="../../style.css"></link>
+  <title>Ajouter un Film</title>
+</head>
+<body>
+<form action="../../admin.php">
+	<input type="submit" value="Retourner à la page d'accueil !">
+</form>
+</body>
