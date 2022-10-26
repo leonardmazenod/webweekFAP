703f8fa723bafec196d33ac6c8f0ea29731ffad7
leonardmazenod <116059783+leonardmazenod@users.noreply.github.com>
2022-10-26 14:41:51 +0200

    ajout favicon

--- ajout/ajouter/ajouter_stand.php
+++ ajout/ajouter/ajouter_stand.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="../../css/style.css"></link>
+  <link rel="icon" href="../../img/icon.png"/>
   <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
   <title>Ajouter un stand</title>
 </head>
