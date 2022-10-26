411ebad2b3add3118cc884e44cf3265cfa3a5004
Julthique <113522369+Julthique@users.noreply.github.com>
2022-10-26 17:49:28 +0200

    Add files via upload

--- ajout/ajouter/ajouter_boutique.php
+++ ajout/ajouter/ajouter_boutique.php
@@ -19,26 +19,43 @@ $descriptionboutique=$_POST['description'];
 $ouvertureboutique=$_POST['ouverture']; 
 $fermetureboutique=$_POST['fermeture']; 
 $telboutique=$_POST['tel']; 
-$imgboutique=$_POST['img'];
 
+$file_min_size = 0;
+$file_max_size = 10000000;
 
-//	Requête qui permet d'insérer toutes les informations que l'on a récupéré dans le formlaire sur le nouveau film	et de les insérer dans la base de données // 
-$reqpreparee = $connection->prepare("INSERT INTO boutique(nomboutique,typeboutique,xcoordboutique,ycoordboutique,descriptionboutique,imgboutique,ouvertureboutique,fermetureboutique,telboutique) VALUES(:nomboutique,:typeboutique,:xcoordboutique,:ycoordboutique,:descriptionboutique,:imgboutique,:ouvertureboutique,:fermetureboutique,:telboutique)");
-$reqpreparee->bindParam(':nomboutique', $nomboutique); 
-$reqpreparee->bindParam(':typeboutique', $typeboutique);
-$reqpreparee->bindParam(':xcoordboutique', $xcoordboutique); 
-$reqpreparee->bindParam(':ycoordboutique', $ycoordboutique); 
-$reqpreparee->bindParam(':descriptionboutique', $descriptionboutique); 
-$reqpreparee->bindParam(':imgboutique', $imgboutique); 
-$reqpreparee->bindParam(':ouvertureboutique', $ouvertureboutique); 
-$reqpreparee->bindParam(':fermetureboutique', $fermetureboutique); 
-$reqpreparee->bindParam(':telboutique', $telboutique); 
+// On vérifie la présence d"un fichier à uploader
+if (($_FILES["fichier"]["size"] > $file_min_size) && ($_FILES["fichier"]["size"] < $file_max_size)) :
+    // dossier où sera déplacé le fichier; ce dossier doit exister
+    $content_dir = "img_bdd";
+    $tmp_file = $_FILES["fichier"]["tmp_name"];
+    if( !is_uploaded_file($tmp_file) ){
+        echo "Image non trouvé";
+    }
+    // on vérifie l"extension
+    $path = $_FILES["fichier"]["name"];
+    $ext = pathinfo($path, PATHINFO_EXTENSION); // on récupère l"extension
+    if(!strstr($ext, "jpg")&& !strstr($ext, "png")&& !strstr($ext, "jpeg")){
+        echo "EXTENSION ".$ext." NON AUTORISEE (extension autorisée : jpg / png / jpeg)";
+    }
+    // Si le formulaire est validé, on copie le fichier dans le dossier de destination
+    if(empty($errors)){
+        $name_file = md5(uniqid(rand(), true)).".".$ext; // on crée un nom unique en conservant l"extension
+        if( !move_uploaded_file($tmp_file, $content_dir . $name_file) ){
+            echo "Il y a des erreurs! Impossible de recupérer l'image";
+        }
+    } 
+    // On récupère l'url du fichier envoyé
+    $get_the_file = $content_dir.$name_file;
+    
+    elseif($_FILES["upfiles"]["size"] > $file_max_size):
+        echo "le fichier dépasse la limite autorisée";
+    else :
+        echo "Pas de fichier joint";
+    endif;
+        $requete='INSERT INTO boutique(nomboutique,typeboutique,xcoordboutique,ycoordboutique,descriptionboutique,imgboutique,ouvertureboutique,fermetureboutique,telboutique) VALUES("'.$nomboutique.'","'.$typeboutique.'","'.$xcoordboutique.'","'.$ycoordboutique.'","'.$descriptionboutique.'","'.$get_the_file.'","'.$ouvertureboutique.'","'.$fermetureboutique.'","'.$telboutique.'")';
+        $requete=$connection->query($requete);
+        $requete->closeCursor();
 
-$succes=$reqpreparee->execute();
-
-if($succes==true){
-	echo '<p>Information de la nouvelle boutique ajoutée.</p>';
-}
 
 ?>
 
@@ -48,3 +65,8 @@ if($succes==true){
 </body>
 <?php include '../../footer.php'; ?>
 </html>
+
+
+
+
+
