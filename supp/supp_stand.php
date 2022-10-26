763b4368a774fa061c01f98c3986c620ad3930a5
leonardmazenod <116059783+leonardmazenod@users.noreply.github.com>
2022-10-26 15:12:42 +0200

    favicon

--- supp/supp_stand.php
+++ supp/supp_stand.php
@@ -4,6 +4,7 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+	<link rel="icon" href="../img/icon.png"/>
   <title>Supprimer un Stand</title>
 </head>
 <body>
