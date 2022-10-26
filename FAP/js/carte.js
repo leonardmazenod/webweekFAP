787eb7218829249be2cef3955b1eced54223cb97
maximetincani <116059733+maximetincani@users.noreply.github.com>
2022-10-26 10:05:05 +0200

    recentrage de la carte

--- FAP/js/carte.js
+++ FAP/js/carte.js
@@ -1,7 +1,7 @@
  fetch('https://france-geojson.gregoiredavid.fr/repo/regions.geojson')
     .then((response) => response.json())
     .then(async (json) => {
-    var map = L.map('map').setView([45.0435, 3.882], 16);
+    var map = L.map('map').setView([45.0415, 3.884], 16);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
     
     // icone textile
