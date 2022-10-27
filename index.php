8404a2c5ac663f4357feb2aaa93d5834521165b0
maximetincani <116059733+maximetincani@users.noreply.github.com>
2022-10-27 14:26:57 +0200

    nouvelle page d'accueil et son css

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
         <script src="https://code.jquery.com/jquery-3.6.1.js"></script> 
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.1/dist/leaflet.css" integrity="sha256-sA+zWATbFveLLNqWO2gtiw3HL/lh1giY/Inf1BJ0z14=" crossorigin=""/>
         <script src="https://unpkg.com/leaflet@1.9.1/dist/leaflet.js" integrity="sha256-NDI0K41gVbWqfkkaHj15IzU7PtMoelkzyKp8TOaFQ3s=" crossorigin=""></script>
+        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     </head>
     <body class="index">
     <!-- Navbar -->
@@ -19,57 +20,178 @@
         include("navbar.php"); 
     ?>
 
-    <!-- Titre -->
-    <div class="titre">
-        <h1>Festival de l’Artisanat </br>au Puy-en-Velay </h1>
-        <h3>du 08 au 10 avril 23</h3>
-        <div class="container">
-            <button>voir les activités</button>
-            <i class="fa-solid fa-arrow-down fa-beat fa-xl"></i>
-        </div>
-    </div>
+    <!-- page index.php, style.css et script.js -->
+	
+                <!-- Titre -->
+                <div class="titre">
+                    <div class="flex-titre" data-aos="fade-up">
+                        <h1>Festival de l’Artisanat</h1>
+                        <div class="festival-desc">
+                            <p>
+                                Le Festival de l’Artisanat est un événement qui valorise toutes les filières de l’artisanat en vous offrant la possibilité de découvrir ou redécouvrir des savoir-faire, des techniques et des produits et services à l’occasion d’une grande fête de l’artisanat se déroulant sur 3 jours. Partez à la rencontre de près de 300 artisans dans tout le centre ville du Puy-en-Velay.
+                            </p>
+                            <div class="iconflex">
+                                <a href="festival.php" id="fleche"><i class="fa-solid fa-arrow-right fa-4"></i></a>
+                                <a href="festival.php" id="ensavoirplus">en savoir plus sur le festival</a>
+                            </div>
+                        </div>
+                    </div>
+                    <h1 class="aupuy" data-aos="fade-up">au Puy-en-Velay </h1>
+                    <h3 data-aos="fade-up">du 08 au 10 avril 2023</h3>
+                    <div class="container">
+                        <a class="grosbouton" href="evenement.php">voir les activités</a>
+                        <div class="btn">
+                            <i class="fa-solid fa-arrow-turn-down fa-xl"></i>
+                        </i>
+                    </div>
+                </div>
+                </div>
+                <div class="artisans"  data-aos="fade-up" data-aos-anchor-placement="center-bottom"><h1>
+                    Nos artisans
+                </h1>
+                <p>
+                    Vous y retrouverez des artisans en tout genre, dans le textile, la sculpture, les plantes et le bois, les bijoux et les pierres précieuses, les travailleurs du métal ou du cuire, ou encore les quelques horloger, vitrailliste et beaucoup d'autre.
+                </p>
+                <p>
+                    Vous retrouverez parmis eux c'est quelques artisans :
+                </p>
+                </div>
+                <!-- Cartes -->
+                <div class="cartes">
+                    <div class="carte">
+                        <a href="evenement.php"><img src="img/1.jpg" alt="Céramiste" data-aos="fade-up"  data-aos-anchor-placement="center-bottom"></a>
+                        <div class="sous-texte" data-aos="fade-up"  data-aos-anchor-placement="center-bottom">
+                            <h4>Céramiste</h4>
+                            <p>
+                                Astrid Legosier
+                                </br>
+                                Atelier de céramique
+                                </br>
+                                de 8h à 19h du samedi au lundi
+                            </p>
+                            <a href="evenement.php">voir plus</a>
+                        </div>
+                    </div>
+                    <div class="carte">
+                        <a href="evenement.php"><img src="img/2.jpg" alt="Tufting" data-aos="fade-up"  data-aos-anchor-placement="center-bottom" data-aos-delay="100"></a>
+                        <div class="sous-texte" data-aos="fade-up"  data-aos-anchor-placement="center-bottom" data-aos-delay="100">
+                            <h4>Tufting</h4>
+                            <p>
+                                Maxime Dumat
+                                </br>
+                                Réalisation de tapis personnalisé
+                                </br>
+                                de 8h à 21h du samedi au lundi
+                            </p>
+                            <a href="evenement.php">voir plus</a>
+                        </div>
+                    </div>
+                    <div class="carte">
+                        <a href="evenement.php"><img src="img/3.jpg" alt="bijoutier" data-aos="fade-up"  data-aos-anchor-placement="center-bottom" data-aos-delay="200"></a>
+                        <div class="sous-texte" data-aos="fade-up"  data-aos-anchor-placement="center-bottom" data-aos-delay="200">
+                            <h4>Bijoutier</h4>
+                            <p>
+                                Lucie Cédiste
+                                </br>
+                                Vente et réalisation de bijoux
+                                </br>
+                                de 8h à 19h du samedi au lundi
+                            </p>
+                            <a href="evenement.php">voir plus</a>
+                        </div>
+                    </div>
+                </div>
 
-    <!-- carousels-->
+                <div class="voirartisans" data-aos="fade-up"  data-aos-anchor-placement="center-bottom">
+                    <a class="grosbouton" href="evenement.php">voir tous les artisans</a>
+                </div>
 
-    <h1 class="fap"> Nos meilleurs artisans </h1>
+                <div class="artisans" data-aos="fade-up"  data-aos-anchor-placement="center-bottom">
+                <h1>
+                    Nos évènements
+                </h1>
+                <p>
+                    Certains de nos artisans proposent de vous montrez leur savoir faire en direct, lors d'évènement ou ils pratiquent leur artisanant devant vous. Retrouvez les aux quatres coins de la ville
+                </p>
+                </div>
+                <!-- carousels-->
+                <div class="swiper-container" data-aos="fade-up"  data-aos-anchor-placement="center-bottom"><div class="swiper-wrapper">
+                    <div class="swiper-slide" style="background: url(./img/serigraphie.jpg) center/cover;">
+                        <div class="sous-texte">
+                            <h4>Sérigraphie</h4>
+                            <p>
+                                Jeremy Faure
+                                </br>
+                                Impression manuel sur tee-shirt
+                                </br>
+                                de 18h à 21h samedi et dimanche
+                            </p>
+                            <a href="evenement.php">voir plus</a>
+                        </div>
+                    </div>
+                    <div class="swiper-slide" style="background: url(./img/bois.PNG) center/cover;">
+                        <div class="sous-texte">
+                            <h4>Sculpture sur bois</h4>
+                            <p>
+                                Michel Auguste
+                                </br>
+                                Réalisation en direct
+                                </br>
+                                de 15h à 19h samedi
+                            </p>
+                            <a href="evenement.php">voir plus</a>
+                        </div>
+                    </div>
+                    <div class="swiper-slide" style="background: url(./img/vitre.jpg) center/cover;">
+                        <div class="sous-texte">
+                            <h4>Vitrailliste</h4>
+                            <p>
+                                Isabelle Carnot
+                                </br>
+                                Création de vitraille
+                                </br>
+                                de 14h à 17h du samedi au lundi
+                            </p>
+                            <a href="evenement.php">voir plus</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-pagination"></div>
+                </div>
+                <script src="js/jquery.min.js" type="text/javascript" ></script>
+                <script src="js/swiper.min.js"></script>
+                <script src="js/carousel.js" type="text/javascript" ></script>
 
-  <div class="swiper-container">
-    <div class="swiper-wrapper">
-      <div class="swiper-slide" style="background: url(./img/bois.jpg) center/cover;"></div>
-      <div class="swiper-slide" style="background: url(./img/verre.jpg) center/cover;"> </div>
-      <div class="swiper-slide" style="background: url(./img/dentelle.jpg) center/cover;"> </div>
-    </div>
-    <div class="swiper-pagination"></div>
-  </div>
+                <div class="voirartisans" data-aos="fade-up"  data-aos-anchor-placement="center-bottom">
+                    <a class="grosbouton" id="map-titre" href="evenement.php">voir tous les évènements</a>
+                </div>
 
-    <!-- Article -->
-    <div class="Article">
-        <h2>Des artisants de toute l’Europe se réunissent pour tenir un festival au Puy-en-Velay du 8 au 10 avril 2023</h2>
-        <img src="img/artisan_petit.jpg">
-        <div class="texte1">
-            <h4>L’artisanat dévoile ses talents</h4>
-            <p>Le Festival de l’Artisanat est un événement qui valorise toutes les filières de l’artisanat en vous offrant la possibilité de découvrir ou redécouvrir des savoir-faire, des techniques et des produits et services à l’occasion d’une grande fête de l’artisanat se déroulant sur 3 jours. Partez à la rencontre de près de 300 artisans dans tout le centre ville du Puy-en-Velay.</p>
-            <button>en savoir plus sur le festival</button>
-        </div>
-    </div>
-   
+                <!-- carte -->
 
-
-  <script src="js/jquery.min.js" type="text/javascript" ></script>
-  <script src="js/swiper.min.js"></script>
-  <script src="js/carousel.js" type="text/javascript" ></script>
-
-        <br><br><br>
-    
-    <h2 id="map-titre">Carte</h2>
-    <div class="carte-flex">
-            <div id="map" class="mapContainer"><script src="js/carte.js"></script></div>
-        </div>
+                <div class="artisans" data-aos="fade-up"  data-aos-anchor-placement="center-bottom">
+                <h1>
+                    Carte
+                </h1>
+                <p>
+                    Retrouvez tous les évènements, les boutiques et les stand sur cette carte interactive.
+                </p>
+                </div>
+                </h1>
+                <div class="carte-flex" data-aos="fade-up"  data-aos-anchor-placement="center-bottom"><div id="map" class="mapContainer">
+                    <script src="js/carte.js"></script>
+                </div>
+                </div>
 
     <!-- Footer -->
     <?php 
         include("footer.php"); 
     ?>
+
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+    AOS.init();
+    </script>
+    <script src="js/script.js"></script>
 
     </body>
 </html>
