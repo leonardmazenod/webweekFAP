1635f6a5104fbfcdfd0cd9b622eff651aaf94f51
bergot <116091613+aubergot@users.noreply.github.com>
2022-10-27 11:36:00 +0200

    Update ajouter_event.php

--- ajout/ajouter/ajouter_event.php
+++ ajout/ajouter/ajouter_event.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" href="../../img/icon.png"/>
-  <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../../bootstrap/css/bootstrap.min.css">
   <title>Ajouter un événement</title>
 </head>
 <body>
