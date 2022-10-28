d9799dee3be690be110abe09ab23225a56eaf6c0
Julthique <113522369+Julthique@users.noreply.github.com>
2022-10-28 10:06:28 +0200

    Add files via upload

--- ajout/ajouter/ajouter_participant.php
+++ ajout/ajouter/ajouter_participant.php
@@ -4,58 +4,58 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" href="../../img/icon.png"/>
-  <link rel="stylesheet" href="../../bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
   <title>Ajouter un participant</title>
 </head>
 <body>
-<?php include '../../navbar.php';?>
+<?php include '../../navbar.php';
 
-  <div class="ajouter_participant">
-  	<?php
-	if (isset($_GET['id'])){
-	  $id_event=$_GET['id'];
-	}
-	else{
-	  $id_event=$_POST['event'];
-	}
+if (isset($_GET['id'])){
+  $id_event=$_GET['id'];
+}
+else{
+  $id_event=$_POST['event'];
+}
 
-	$connection=new PDO('mysql:host=localhost;port=3306;dbname=festival','root','');
-	$nom=$_POST['nom']; 
-	$prenom=$_POST['prenom']; 
-	$email=$_POST['email']; 
-	$tel=$_POST['tel']; 
+$connection=new PDO('mysql:host=localhost;port=3306;dbname=festival','root','');
+$nom=$_POST['nom']; 
+$prenom=$_POST['prenom']; 
+$email=$_POST['email']; 
+$tel=$_POST['tel']; 
 
 
 
-	//	Requête qui permet d'insérer toutes les informations que l'on a récupéré dans le formlaire sur le nouveau film	et de les insérer dans la base de données // 
-	$reqpreparee = $connection->prepare("INSERT INTO participant(nom_participant,prenom_participant,email_participant,tel_participant) VALUES(:nom_participant,:prenom_participant,:email_participant,:tel_participant)");
-	$reqpreparee->bindParam(':nom_participant', $nom); 
-	$reqpreparee->bindParam(':prenom_participant', $prenom);
-	$reqpreparee->bindParam(':email_participant', $email); 
-	$reqpreparee->bindParam(':tel_participant', $tel); 
+//	Requête qui permet d'insérer toutes les informations que l'on a récupéré dans le formlaire sur le nouveau film	et de les insérer dans la base de données // 
+$reqpreparee = $connection->prepare("INSERT INTO participant(nom_participant,prenom_participant,email_participant,tel_participant) VALUES(:nom_participant,:prenom_participant,:email_participant,:tel_participant)");
+$reqpreparee->bindParam(':nom_participant', $nom); 
+$reqpreparee->bindParam(':prenom_participant', $prenom);
+$reqpreparee->bindParam(':email_participant', $email); 
+$reqpreparee->bindParam(':tel_participant', $tel); 
 
-	$succes=$reqpreparee->execute();
+$succes=$reqpreparee->execute();
 
-	if($succes==true){
-		echo '<p>Inscription confirmée avec succès</p>';
-	}
+if($succes==true){
+	echo '<p>Inscription confirmée avec succès</p>';
+}
 
-	// On récupére l'id du nouveau film que l'on vient d'ajouter  //
-	$id_participant=$connection->lastInsertId();
+// On récupére l'id du nouveau film que l'on vient d'ajouter  //
+$id_participant=$connection->lastInsertId();
 
-	$reqpreparee = $connection->prepare("INSERT IGNORE INTO va_participer(id_event,id_participant) VALUES(:id_event,:id_participant)");
-	$reqpreparee->bindParam(':id_event', $id_event); 
-	$reqpreparee->bindParam(':id_participant', $id_participant);			
-	$succes=$reqpreparee->execute();
+$reqpreparee = $connection->prepare("INSERT IGNORE INTO va_participer(id_event,id_participant) VALUES(:id_event,:id_participant)");
+$reqpreparee->bindParam(':id_event', $id_event); 
+$reqpreparee->bindParam(':id_participant', $id_participant);			
+$succes=$reqpreparee->execute();
+
+if($succes==true){
+	echo ' !';
+}
+
+?>
 
 
-	?>
-
-
-		<form action="../../index.php">
-			<input type="submit" class="btn btn-primary green" value="Retourner à la page d'accueil !">
-		</form>
-	</div>
+<form action="../../admin.php">
+	<input type="submit" class="btn btn-primary green" value="Retourner à la page d'accueil !">
+</form>
 </body>
 <?php include '../../footer.php'; ?>
 </html>
--- ajout/ajouter/ajouter_stand.php
+++ ajout/ajouter/ajouter_stand.php
@@ -9,45 +9,66 @@
 </head>
 <body>
 <?php include '../../navbar.php';?>
+<div class="ajouter_event">
+<?php
+$connection=new PDO('mysql:host=localhost;port=3306;dbname=festival','root','');
+$nomstand=$_POST['nom']; 
+$typestand=$_POST['type']; 
+$xcoordstand=$_POST['xcoord']; 
+$ycoordstand=$_POST['ycoord']; 
+$descriptionstand=$_POST['description']; 
+$ouverturestand=$_POST['ouverture']; 
+$fermeturestand=$_POST['fermeture']; 
+$emplacement=$_POST['emplacement']; 
 
-<div class="ajouter_stand">
-  <?php
-  $connection=new PDO('mysql:host=localhost;port=3306;dbname=festival','root','');
-  $nomstand=$_POST['nom']; 
-  $typestand=$_POST['type']; 
-  $xcoordstand=$_POST['xcoord']; 
-  $ycoordstand=$_POST['ycoord']; 
-  $descriptionstand=$_POST['description']; 
-  $ouverturestand=$_POST['ouverture']; 
-  $fermeturestand=$_POST['fermeture']; 
-  $emplacement=$_POST['emplacement']; 
-  $imgstand=$_POST['img'];
+$file_min_size = 0;
+$file_max_size = 10000000;
+
+// On vérifie la présence d"un fichier à uploader
+if (($_FILES["img"]["size"] > $file_min_size) && ($_FILES["img"]["size"] < $file_max_size)) :
+    // dossier où sera déplacé le fichier; ce dossier doit exister
+    $content_dir = "img_bdd/";
+    $tmp_file = $_FILES["img"]["tmp_name"];
+    if( !is_uploaded_file($tmp_file) ){
+        echo "Image non trouvé";
+    }
+    // on vérifie l"extension
+    $path = $_FILES["img"]["name"];
+    $ext = pathinfo($path, PATHINFO_EXTENSION); // on récupère l"extension
+    if(!strstr($ext, "jpg")&& !strstr($ext, "png")&& !strstr($ext, "jfif")&& !strstr($ext, "jpeg")){
+        echo "EXTENSION ".$ext." NON AUTORISEE (extension autorisée : jpg / png / jpeg)";
+    }
+    // Si le formulaire est validé, on copie le fichier dans le dossier de destination
+    if(empty($errors)){
+        $name_file = md5(uniqid(rand(), true)).".".$ext; // on crée un nom unique en conservant l"extension
+        if( !move_uploaded_file($tmp_file, $content_dir . $name_file) ){
+            echo "Il y a des erreurs! Impossible de recupérer l'image";
+        }
+    } 
+    // On récupère l'url du fichier envoyé
+    $get_the_file = $content_dir.$name_file;
+    
+    elseif($_FILES["upfiles"]["size"] > $file_max_size):
+        echo "le fichier dépasse la limite autorisée";
+    else :
+        echo "Pas de fichier joint";
+    endif;
+        $requete='INSERT INTO stand(nomstand,typestand,xcoordstand,ycoordstand,descriptionstand,imgstand,ouverturestand,fermeturestand,emplacement) VALUES("'.$nomstand.'","'.$typestand.'","'.$xcoordstand.'","'.$ycoordstand.'","'.$descriptionstand.'","'.$get_the_file.'","'.$ouverturestand.'","'.$fermeturestand.'","'.$emplacement.'")';
+        $requete=$connection->query($requete);
+        $requete->closeCursor();
 
 
-  //	Requête qui permet d'insérer toutes les informations que l'on a récupéré dans le formlaire sur le nouveau film	et de les insérer dans la base de données // 
-  $reqpreparee = $connection->prepare("INSERT INTO stand(nomstand,typestand,xcoordstand,ycoordstand,descriptionstand,imgstand,ouverturestand,fermeturestand,emplacement) VALUES(:nomstand,:typestand,:xcoordstand,:ycoordstand,:descriptionstand,:imgstand,:ouverturestand,:fermeturestand,:emplacement)");
-  $reqpreparee->bindParam(':nomstand', $nomstand); 
-  $reqpreparee->bindParam(':typestand', $typestand);
-  $reqpreparee->bindParam(':xcoordstand', $xcoordstand); 
-  $reqpreparee->bindParam(':ycoordstand', $ycoordstand); 
-  $reqpreparee->bindParam(':descriptionstand', $descriptionstand); 
-  $reqpreparee->bindParam(':imgstand', $imgstand); 
-  $reqpreparee->bindParam(':ouverturestand', $ouverturestand); 
-  $reqpreparee->bindParam(':fermeturestand', $fermeturestand); 
-  $reqpreparee->bindParam(':emplacement', $emplacement); 
+?>
 
-  $succes=$reqpreparee->execute();
-
-  if($succes==true){
-    echo '<p> Information du nouveaux stand ajoutée.</p>';
-  }
-
-  ?>
-
-  <form action="../../index.php">
-    <input type="submit" class="btn btn-primary green" value="Retourner à la page d'accueil !">
-  </form>
-</div>
+<form action="../../admin.php">
+	<input type="submit" class="btn btn-primary green" value="Retourner à la page d'accueil !">
+</form>
+  </div>
 </body>
 <?php include '../../footer.php'; ?>
 </html>
+
+
+
+
+
