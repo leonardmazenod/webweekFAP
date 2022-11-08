130c04537543bcb3528825ec865c75a0482d0e97
Julthique <113522369+Julthique@users.noreply.github.com>
2022-11-08 15:23:38 +0100

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content=width=device-width, initial-scale=1>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <title>Festival de l'Artisanat au Puy</title>
+        <link rel="icon" href="img/icon.png"/>
+        <link rel="stylesheet" href="js/swiper.min.css">
+        <link rel="stylesheet" href="css/style.css"/>
+        <script src="https:/kit.fontawesome.com/37f50f5103.js" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.6.1.js"></script> 
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.1/dist/leaflet.css" integrity="sha256-sA+zWATbFveLLNqWO2gtiw3HL/lh1giY/Inf1BJ0z14=" crossorigin=""/>
+        <script src="https://unpkg.com/leaflet@1.9.1/dist/leaflet.js" integrity="sha256-NDI0K41gVbWqfkkaHj15IzU7PtMoelkzyKp8TOaFQ3s=" crossorigin=""></script>
+        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    </head>
+    <body class="index">
+
+    <div>désolé nous rencontrons une erreur.<div>
+    <imp src="img/404.png">
+
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+    AOS.init();
+    </script>
+    <script src="js/script.js"></script>
+
+    </body>
+</html>
