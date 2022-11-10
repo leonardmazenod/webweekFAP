0c42fbca8dd920d3ede9c9e62b966a5403f563b6
maximetincani <116059733+maximetincani@users.noreply.github.com>
2022-11-10 11:57:41 +0100

    Add files via upload

--- inscription.php
+++ inscription.php
@@ -15,82 +15,63 @@ $resultats->closeCursor();
 $nbevent=count($tab_evenement);
 ?>
 
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="author" content="OTILEMA-WEB" />
-  <meta name="description" content="cette page est utile pour s'inscrire" />
-  <meta name= "keywords" content= "inscription,Nom,prénom,E-mail,telephone">
-  <link rel="icon" href="img/icon.png">
-  <link rel="stylesheet" href="css/style.css">
-  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-  <title>Inscription</title>
-</head>
-<body>
+<?php 
+	$title="Page inscription";
+	$description="cette page est utile pour s'inscrire";
+	$bootstrap=true;
+	include("navbar.php"); 
+?>
 
+<div class="allpage page-inscription">
 	<?php 
-        include("navbar.php"); 
-    ?>
-
-	<div class="page-inscription">
-		<?php 
-		if ($id!=0){
-		echo '<h1>Inscription à l\'événement :'. $tab_event[0].'</h1>';
-		echo '<form method="POST" action="ajout/ajouter/ajouter_participant.php?id='.$id.'">';
+	if ($id!=0){
+	echo '<h1>Inscription à l\'événement :'. $tab_event[0].'</h1>';
+	echo '<form method="POST" action="ajout/ajouter/ajouter_participant.php?id='.$id.'">';
+	}
+	else{
+		echo '<h1>Inscription :</h1>';
+		echo '<form method="POST" action="ajout/ajouter/ajouter_participant.php">';
+		echo '<div class="col-md-8">
+		<label for="event">Événement : </label>
+		<select name="event" class="form-select" id="event">';
+		for ($i=0;$i<$nbevent;$i++){
+			echo "<option value=".$tab_evenement[$i]["id_event"].">".$tab_evenement[$i]["nomevent"]."</option>";
 		}
-		else{
-			echo '<h1>Inscription :</h1>';
-			echo '<form method="POST" action="ajout/ajouter/ajouter_participant.php">';
-			echo '<div class="col-md-8">
-			<label for="event">Événement : </label>
-			<select name="event" class="form-select" id="event">';
-			for ($i=0;$i<$nbevent;$i++){
-				echo "<option value=".$tab_evenement[$i]["id_event"].">".$tab_evenement[$i]["nomevent"]."</option>";
-			}
-			
-			echo'</select></div>';
-		}
+		
+		echo'</select></div>';
+	}
 
-		?>
-		<div class="row gd-3">
-			<div class="col-md-4">
-				<label for="nom">Nom : </label>
-				<input type="text" name="nom" id="nom" class="form-control" required="required" placeholder="Nom de famille">
-			</div>
-			<div class="col-md-4">
-				<label for="prenom">Prénom : </label>
-				<input type="text" name="prenom" id="prenom" class="form-control" required="required" placeholder="Prénom">	
-			</div>
+	?>
+	<div class="row gd-3">
+		<div class="col-md-4">
+			<label for="nom">Nom : </label>
+			<input type="text" name="nom" id="nom" class="form-control" required="required" placeholder="Nom de famille">
 		</div>
-		<div class="row gd-3">
-			<div class="col-md-4">
-				<label for="email">E-mail : </label>
-				<input type="text" name="email" id="email" class="form-control" required="required" placeholder="adresse@mail.com">
-			</div>
-			<div class="col-md-4">
-				<label for="tel">Téléphone : </label>
-				<input type="text" id="tel" name="tel" class="form-control" required="required" placeholder="0601020304">
-			</div>
+		<div class="col-md-4">
+			<label for="prenom">Prénom : </label>
+			<input type="text" name="prenom" id="prenom" class="form-control" required="required" placeholder="Prénom">	
 		</div>
-			<div>
-				<input type="submit" class="btn btn-primary green" value="Confirmer l'incription">
-			</div>
-		</form>
-		<form action="evenement.php">
-			<div>
-			<input type="submit" class="btn btn-primary green" value="Annuler">
-			</div>
-		</form>
-		<form action="contact.php" method="POST">
-			<div>
-			<input type="hidden" name="x" value="0" >
-			<input type="submit" class="btn btn-primary green" value="Annuler mon inscription">
-			</div>
-		</form>
 	</div>
-	  <!-- Footer -->
-	  <?php include("footer.php"); ?>
-</body>
-</html>
+	<div class="row gd-3">
+		<div class="col-md-4">
+			<label for="email">E-mail : </label>
+			<input type="text" name="email" id="email" class="form-control" required="required" placeholder="adresse@mail.com">
+		</div>
+		<div class="col-md-4">
+			<label for="tel">Téléphone : </label>
+			<input type="text" id="tel" name="tel" class="form-control" required="required" placeholder="0601020304">
+		</div>
+	</div>
+		<div>
+			<input type="submit" class="btn btn-primary green" value="Confirmer l'incription">
+		</div>
+	</form>
+	<form action="contact.php" method="POST">
+		<div>
+		<input type="hidden" name="x" value="0" >
+		<input type="submit" class="btn btn-primary green" value="Annuler mon inscription">
+		</div>
+	</form>
+</div>
+	<!-- Footer -->
+	<?php include("footer.php"); ?>
