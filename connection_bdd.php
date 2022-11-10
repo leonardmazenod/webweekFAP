13a3205142841f4b77a11f0a6fb0b31ea3b417fa
maximetincani <116059733+maximetincani@users.noreply.github.com>
2022-11-10 12:10:47 +0100

    Update connection_bdd.php

--- connection_bdd.php
+++ connection_bdd.php
@@ -1,3 +1,3 @@
 <?php
-$connection=new PDO('mysql:host=localhost;port=3306;dbname=festival','root','root');
+$connection=new PDO('mysql:host=localhost;port=3306;dbname=thsouvigne_fap','tFAP43admin','thsouvigne_fap');
 ?>
