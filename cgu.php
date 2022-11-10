273bf3f80892dded6a023ff3d523785de456cbbf
leonardmazenod <116059783+leonardmazenod@users.noreply.github.com>
2022-11-10 10:19:49 +0100

    Add files via upload

---
+++ cgu.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content=width=device-width, initial-scale=1>
+        <meta name="description" content="Cette page est une page de mentions legales et de conditions générale d'utilisation"/>
+        <meta name="keywords" content="mentions, legales, conditions, utilisation, fap, festival, artisanat"/>
+        <meta name="author" content="Otiléma-web"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <title>CGU / Mentions Légales</title>
+        <link rel="stylesheet" href="css/style.css"/>
+        <link rel="icon" href="img/icon.png"/>
+    </head>
+    <body>
+        <!-- Navbar -->
+        <?php 
+            include("navbar.php"); 
+        ?>
+        <h1>Conditions générales d'utilisation du site fap.mmi-lepuy.fr</h1>
+        <div>
+            <h2>ARTICLE 1 : Objet</h2>
+            <p>Les présentes « conditions générales d'utilisation » ont pour objet l'encadrement juridique de l’utilisation du site fap.mmi-lepuy.fr et de ses services.
+                Ce contrat est conclu entre :
+                Le gérant du site internet, ci-après désigné « l’Éditeur »,
+                Toute personne physique ou morale souhaitant accéder au site et à ses services, ci-après appelé « l’Utilisateur ».
+                Les conditions générales d'utilisation doivent être acceptées par tout Utilisateur, et son accès au site vaut acceptation de ces conditions.
+            </p>
+        </div>
+        <div>
+            <h2>ARTICLE 2 : Mentions légales</h2>
+            <p>Le site fap.mmi-lepuy.fr est édité par le groupe FAP, domicilié au 5 rue Jean Baptiste Fabre.</p>
+        </div>
+        <div>
+            <h2>ARTICLE 3 : accès aux services</h2>
+            <p>L’Utilisateur du site fap.mmi-lepuy.fr a accès aux services suivants :
+                •	Renseignement
+                •	Inscription
+                Tout Utilisateur ayant accès à internet peut accéder gratuitement et depuis n’importe où au site. Les frais supportés par l’Utilisateur pour y accéder (connexion internet, matériel informatique, etc.) ne sont pas à la charge de l’Éditeur.
+                Le site et ses différents services peuvent être interrompus ou suspendus par l’Éditeur, notamment à l’occasion d’une maintenance, sans obligation de préavis ou de justification.
+            </p>
+        </div>
+        <div>
+            <h2>ARTICLE 4 : Responsabilité de l’Utilisateur</h2>
+            <p>L’Utilisateur assume l’entière responsabilité de l’utilisation qu’il fait des informations et contenus présents sur le site fap.mmi-lepuy.fr.
+                Tout usage du service par l'Utilisateur ayant directement ou indirectement pour conséquence des dommages doit faire l'objet d'une indemnisation au profit du site.
+                Le membre s’engage à tenir des propos respectueux des autres et de la loi et accepte que ces publications soient modérées ou refusées par l’Éditeur, sans obligation de justification. 
+            </p>
+        </div>
+        <div>
+            <h2>ARTICLE 5 : Responsabilité de l’Éditeur</h2>
+            <p>Tout dysfonctionnement du serveur ou du réseau ne peut engager la responsabilité de l’Éditeur.
+                De même, la responsabilité du site ne peut être engagée en cas de force majeure ou du fait imprévisible et insurmontable d'un tiers.
+                Le site fap.mmi-lepuy.fr s'engage à mettre en œuvre tous les moyens nécessaires pour garantir la sécurité et la confidentialité des données. Toutefois, il n’apporte pas une garantie de sécurité totale.
+                L’Éditeur se réserve la faculté d’une non-garantie de la fiabilité des sources, bien que les informations diffusées sur le site soient réputées fiables.
+            </p>
+        </div>
+        <div>
+            <h2>ARTICLE 6 : Propriété intellectuelle</h2>
+            <p>Les contenus du site fap.mmi-lepuy.fr (logos, textes, éléments graphiques, vidéos, etc.) sont protégés par le droit d’auteur, en vertu du Code de la propriété intellectuelle.
+                L’Utilisateur devra obtenir l’autorisation de l’éditeur du site avant toute reproduction, copie ou publication de ces différents contenus.
+                Ces derniers peuvent être utilisés par les utilisateurs à des fins privées ; tout usage commercial est interdit.
+                L’Utilisateur est entièrement responsable de tout contenu qu’il met en ligne et il s’engage à ne pas porter atteinte à un tiers.
+            </p>
+        </div>
+        <div>
+            <h2>ARTICLE 7 : Données personnelles</h2>
+            <p>L’Utilisateur doit obligatoirement fournir des informations personnelles pour procéder à son inscription sur le site. 
+                L’adresse électronique (e-mail) de l’utilisateur pourra notamment être utilisée par le site fap.mmi-lepuy.fr pour la communication d’informations diverses et la gestion du compte.
+                fap.mmi-lepuy.fr garantie le respect de la vie privée de l’utilisateur, conformément à la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés.
+                En vertu des articles 39 et 40 de la loi en date du 6 janvier 1978, l'Utilisateur dispose d'un droit d'accès, de rectification, de suppression et d'opposition de ses données personnelles. L'Utilisateur exerce ce droit via :
+                    •	Son espace personnel sur le site ;
+                    •	Un formulaire de contact ;
+                    •	Par mail à festivalartisanatpuyenvelay@gmail.com;
+            </p>
+        </div>
+        <div>
+            <h2>ARTICLE 8 : Liens hypertextes</h2>
+            <p>Les domaines vers lesquels mènent les liens hypertextes présents sur le site n’engagent pas la responsabilité de l’Éditeur de fap.mmi-lepuy.fr, qui n’a pas de contrôle sur ces liens.
+                Il est possible pour un tiers de créer un lien vers une page du site fap.mmi-lepuy.fr sans autorisation expresse de l’éditeur.
+            </p>
+        </div>
+        <div>
+            <h2>ARTICLE 9 : Évolution des conditions générales d’utilisation</h2>
+            <p>Le site fap.mmi-lepuy.fr se réserve le droit de modifier les clauses de ces conditions générales d’utilisation à tout moment et sans justification.</p>
+        </div>
+        <div>
+            <h2>ARTICLE 10 : Durée du contrat</h2>
+            <p>La durée du présent contrat est indéterminée. Le contrat produit ses effets à l'égard de l'Utilisateur à compter du début de l’utilisation du service.</p>
+        </div>
+        <div>
+            <h2>ARTICLE 11 : Droit applicable et juridiction compétente</h2>
+            <p>Le présent contrat dépend de la législation française. 
+                En cas de litige non résolu à l’amiable entre l’Utilisateur et l’Éditeur, les tribunaux de Le-Puy-En-Velay sont compétents pour régler le contentieux.
+            </p>
+        </div>
+            <!-- Footer -->
+    <?php 
+        include("footer.php"); 
+    ?>
+    </body>
+</html>
